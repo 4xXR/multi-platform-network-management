406fd165847de515b5861d03347c985cee70a136
Daniel <danyurru92@gmail.com>
2024-09-15 20:14:05 -0400

    Get interfaces state and generate json file

--- scripts/restconf-get.py
+++ scripts/restconf-get.py
@@ -9,16 +9,30 @@ router = {"ip": "devnetsandboxiosxe.cisco.com", "port": "443",
 headers = {"Accept": "application/yang-data+json",
            "Content-Type": "application/yang-data+json"}
 
-url = f"https://{router['ip']}:{router['port']}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/interface=GigabitEthernet1"
+url = f"https://{router['ip']}:{router['port']}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/"
 # print(url)
 
 response = requests.get(url, headers=headers, auth=(
     router['user'], router['password']), verify=False)
 
-
+#Estado de las interfaces
 api_data = response.json()
-print("/" * 50)
-print(api_data["Cisco-IOS-XE-interfaces-oper:interface"][0].get('interface-type'))
-print("/" * 50)
-if api_data["Cisco-IOS-XE-interfaces-oper:interface"][0].get("admin-status") == 'if-state-up':
-    print('Interface is up')
+interfaces = api_data["Cisco-IOS-XE-interfaces-oper:interfaces"]['interface']
+for interface in interfaces:
+    print('Name: ' + interface['name'])
+    print('Description: ' + interface['description'])
+    
+    if interface['admin-status'] == 'if-state-up':
+        print('Interface is up')
+    else:
+        print('Interface is down')
+    print('/' * 20)
+
+# url = f"https://{router['ip']}:{router['port']}/restconf/data/Cisco-IOS-XE-native:native/"
+# response = requests.get(url, headers=headers, auth=(
+#     router['user'], router['password']), verify=False)
+# api_data = response.json()
+# print(api_data['Cisco-IOS-XE-native:native']['ip']['route'])
+
+with open('data/interface_data.json', 'w') as json_file:
+    json.dump(api_data, json_file, indent = 4)
