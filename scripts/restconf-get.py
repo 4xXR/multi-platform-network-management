aa95d31960b92bd9cbd75e7a76b564ff03600da5
Daniel <danyurru92@gmail.com>
2024-10-21 14:38:04 -0400

    Adding code for SD-WAN auth

--- scripts/restconf-get.py
+++ scripts/restconf-get.py
@@ -1,20 +1,80 @@
-import datetime
+from datetime import datetime
 import requests
 import json
 import os
+import urllib3
 
+urllib3.disable_warnings()
 
+class Authentication():
+    @staticmethod
+    def get_jsessionid(vmanage_host, vmanage_port, username, password):
+        api = "/j_security_check"
+        base_url = f"https://{vmanage_host}:{vmanage_port}"
+        url = base_url + api
+        payload = {'j_username': username, 'j_password': password}
+
+        response = requests.post(url=url, data=payload, verify=False)
+        try:
+            cookies = response.headers["set-Cookie"]
+            jsessionid = cookies.split(";")[0]
+            return jsessionid
+        except KeyError:
+            print("No valid JSESSION ID returned")
+            exit()
+
+    @staticmethod
+    def get_token(vmanage_host, vmanage_port, jsessionid):
+        headers = {'Cookie': jsessionid}
+        base_url = f"https://{vmanage_host}:{vmanage_port}"
+        api = "/dataservice/client/token"
+        url = base_url + api
+        response = requests.get(url=url, headers=headers, verify=False)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
 
 #Load the device config
 def load_device_config(file_path):
     with open(file_path,'r') as file_config:
         return json.load(file_config)
-   
+
+# # Function to obtain an authentication token
+# def get_auth_token(device_config):
+#     url = f"https://{device_config['ip']}:{device_config['port']}/j_security_check"
+#     payload = {
+#         "j_username": device_config['user'],
+#         "j_password": device_config['password']
+#     }
+#     headers = {
+#         "Content-Type": "application/x-www-form-urlencoded"
+#     }
+#     response = requests.post(url, data=payload, headers=headers, verify=False)
+
+#     try:
+#         session = requests.Session()
+#         response = session.post(url, data=payload, verify=False)
+#         if response.status_code == 200 and 'JSESSIONID' in session.cookies:
+#             return session.cookies['JSESSIONID']
+#         else:
+#             print("Failed to obtain token. Check your credentials.")
+#             return None
+#     except requests.exceptions.RequestException as err:
+#         print(f"Error occurred during token retrieval: {err}")
+#         return None
+
 #Get device data
-def get_device_data(device_config,headers, endpoint):
+def get_device_data(device_config, headers, endpoint, auth_type='basic'):
     url = f"https://{device_config['ip']}:{device_config['port']}/restconf/data/{endpoint}/"
+    #url = f"https://{device_config['ip']}:{device_config['port']}/dataservice/device/tunnel/{endpoint}"
     try:
-        response = requests.get(url, headers=headers, auth=(device_config['user'], device_config['password']), verify=False)
+        if auth_type == 'token':
+            headers['Cookie'] = f"JSESSIONID={device_config['token']}"
+            response = requests.get(url, headers=headers, verify=False)
+        else: # Assume basic authentication
+            response = requests.get(url, headers=headers, auth=(device_config['user'], device_config['password']), verify=False)
+        
         response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
         return response.json()
     except requests.exceptions.HTTPError as http_err:
@@ -36,28 +96,47 @@ def save_data_to_file(data,file_data_path):
 
 #Main function to coordinate the operation
 def run_data_collection(device_config_file, endpoint):
-    # Cargar configuración del dispositivo desde el archivo
+    # Load the device configuration from the file
     device_config = load_device_config(device_config_file)
     
-    # Definir los headers RESTCONF
-    headers = {"Accept": "application/yang-data+json",
-               "Content-Type": "application/yang-data+json"}
-    
-    # Obtener los datos del dispositivo
-    api_data = get_device_data(device_config,headers, endpoint)
+# Check for token-based authentication
+    if device_config.get('auth_type') == 'token':
+        Auth = Authentication()
+        jsessionid = Auth.get_jsessionid(device_config['ip'], device_config['port'], device_config['user'], device_config['password'])
+        token = Auth.get_token(device_config['ip'], device_config['port'], jsessionid)
+        
+        if token:
+            headers = {
+                "Accept": "application/yang-data+json",
+                "Content-Type": "application/yang-data+json",
+                "X-Auth-Token": token  # Agrega el token a los headers para la autenticación
+            }
+            auth_type = 'token'
+
+        else:
+            print("Error: No se pudo obtener el token de autenticación.")
+            return  # Termina la ejecución si no se pudo obtener el token
+    else:
+        # Definir los headers RESTCONF para autenticación básica
+        headers = {"Accept": "application/yang-data+json", "Content-Type": "application/yang-data+json"}
+        auth_type = 'basic'
+
+        # Obtener los datos del dispositivo
+    api_data = get_device_data(device_config,headers, endpoint, auth_type)
     
     # Obtener la fecha actual para nombrar el archivo de salida
     current_date = datetime.now().strftime("%Y-%m-%d")
-    file_name = f"interface_data_{current_date}.json"
+    file_name = f"interface_data_sdwan2{current_date}.json"
     
     # Guardar los datos en un archivo
     save_data_to_file(api_data, file_name)
     
-    print(f"Datos guardados en: data/{file_name}")
+    print(f"Data stored in: data/{file_name}")
 
-# file_path = 'config/cat8000v_config.json'
-# device_config = load_device_config(file_path)
-# #print(device_config['ip'])
-# global_device_config_file = run_data_collection(device_config_file)
-# #Execute data collection
-# run_data_collection(device_config_file, endpoint)
+#file_path = 'config/sdwan_config.json'
+#device_config = load_device_config(file_path)
+#print(device_config['ip'])
+device_config_file = 'config/cat8000v_config.json'
+endpoint = 'Cisco-IOS-XE-interfaces-oper:interface'
+# Execute data collection
+run_data_collection(device_config_file, endpoint)
